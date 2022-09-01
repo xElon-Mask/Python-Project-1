6a22d8ae73845ee20f5a3727bf049c731904c2cc
xElon-Mask <xelonmask@protonmail.com>
2022-09-01 12:08:53 +0200

    create function demander_age()

--- main.py
+++ main.py
@@ -2,12 +2,25 @@
 Formation Python
 Apprendre la programmation """
 
-# redemander le nom tant que l'user n'a pas répondu à la question, testez juste si le nom est vide ou non
+
+def demander_age():
+    age_int = 0
+    while age_int == 0:
+        age_str = input("Quel est votre age ?")
+        try:
+            age_int = int(age_str)
+        except:
+            print("ERREUR : vous devez rentrer un nombre pour l'age")
+    return age_int
+
+# demander le nom
 nom = ""
 while nom == "":
     nom = input("Quel est votre nom ?")
 
-age = 0
+# demander l'age
+age = demander_age()
+
 
 """"try veut dire essayer en anglais, 
 le terme est utilisé en programmation pour gérer des erreurs possibles.
@@ -15,13 +28,8 @@ Si l'instruction sous try n'a pas fonctionné, il faut donc une exception qui s'
 Pour éviter la suite des instructions après avoir pris l'exception, il faut passer les instructions post-exception dans un bloc else:
 
 while veut dire "tant que" """
-while age == 0:
-    age_str = input("Quel est votre age ?")
-    try:
-        age = int(age_str)
-    except:
-        print("ERREUR : vous devez rentrer un nombre pour l'age")
 
+# afficher les résultats
 print("Vous vous appelez " + nom + ", vous avez " + str(age) + " ans")
 # ceci est un commentaire en Python
 print("L'an prochain vous aurez " + str(age+1) + " ans")
